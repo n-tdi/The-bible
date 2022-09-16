ec486a3b1c943a6e48e895282e7f05e2c1df80e6
professional-tdi <yogikngyt@gmail.com>
2022-09-16 10:44:05 -0400

    Updated api

--- src/main/java/world/ntdi/custombooks/BookCommand.java
+++ src/main/java/world/ntdi/custombooks/BookCommand.java
@@ -2,6 +2,7 @@ package world.ntdi.custombooks;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -12,41 +13,39 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class BookCommand implements CommandExecutor {
+    private static List<String> Godspel = new LinkedList<>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player p) {
             ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+            translate(1);
 
             BookMeta meta = (BookMeta) book.getItemMeta();
             meta.setTitle(ChatColor.RED + "Genesis");
             meta.setAuthor(ChatColor.MAGIC + "God");
-            meta.addPage("In the beginning God created the heavens and the earth.",
-                    "Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters.",
-                    "And God said, “Let there be light,” and there was light.",
-                    "God saw that the light was good, and he separated the light from the darkness.",
-                    "God called the light “day,” and the darkness he called “night.” And there was evening, and there was morning—the first day."
-            );
+            meta.addPage(Godspel.get(0));
 
             book.setItemMeta(meta);
             p.getInventory().addItem(book);
-
-            translate();
-
 
         }
         return true;
     }
 
-    public void translate(int start)  {
-        if (start > 29) return;
-        HttpResponse<String> response = Unirest.get("https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter?Verse=2&chapter=1&Book=Genesis")
+    public static void translate(int chapt)  {
+        HttpResponse<String> response = Unirest.get("https://ajith-holy-bible.p.rapidapi.com/GetChapter?chapter=" + chapt + "&Book=Genesis")
                 .header("X-RapidAPI-Key", "5867a41e4fmsh9e33ed99bda2765p116b97jsnd5148eac9ead")
                 .header("X-RapidAPI-Host", "ajith-holy-bible.p.rapidapi.com")
                 .asString();
 
-        System.out.println(response.getBody());
+        JSONObject jsonObject = new JSONObject(response.getBody());
+        Godspel.add(jsonObject.getString("Output"));
     }
 
 }
